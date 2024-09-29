09e2624e3ff8a7c04c83e0a98d46a80e0abd659e
sebastian <sebasruizr05@gmail.com>
2024-09-29 12:43:31 -0500

    Cambios 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,12 @@ import { LoginComponent } from './login/login.component';
 import { SignupComponent } from "./signup/signup.component";
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './layout/footer/footer.component';
-import { HeaderComponent } from "./layout/header/header.component";
-import { FormsModule } from '@angular/forms'; // Asegúrate de importar FormsModule
+import { HeaderComponent } from "./layout/header/header.component"; // Verifica la ruta del header
+import { FormsModule } from '@angular/forms';
 
 @Component({
   standalone: true,
-  imports: [RouterOutlet, LoginComponent, SignupComponent, FooterComponent, HeaderComponent, FormsModule], // Incluye FormsModule aquí
+  imports: [RouterOutlet, LoginComponent, SignupComponent, FooterComponent, HeaderComponent, FormsModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  menuOpen = false;
 
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  // Cierra el menú si haces clic fuera del icono o del menú
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: Event) {
+    const target = event.target as HTMLElement;
+    if (!target.closest('.menu') && this.menuOpen) {
+      this.menuOpen = false;
+    }
+  }
 }
