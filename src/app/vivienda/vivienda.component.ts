3b7d8fb762e86c62222674705c82732db4ac487d
yos <ysmaprogramming@gmail.com>
2024-09-30 12:42:36 -0500

    buscar propiedades

--- src/app/vivienda/vivienda.component.ts
+++ src/app/vivienda/vivienda.component.ts
@@ -4,28 +4,38 @@ import { HeaderComponent } from "../layout/header/header.component";
 import { ViviendaService } from '../services/vivienda.service';
 import { vivienda } from '../models/property.model';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-vivienda',
   standalone: true,
-  imports: [FooterComponent, HeaderComponent],
+  imports: [FooterComponent, HeaderComponent, FormsModule],
   templateUrl: './vivienda.component.html',
   styleUrl: './vivienda.component.css'
 })
 export class ViviendaComponent {
+  query = ""
+  viviendas: vivienda[] = []
+  constructor(private router: Router, private viviendaService: ViviendaService) {
+    this.viviendas = this.viviendaService.getViviendas()
+   }
 
-
-  constructor(private router: Router, private viviendaService: ViviendaService) { }
-
-  obetenerViviendas() {
+  viviendasDestacadas() {
     return this.viviendaService.getViviendas()
   }
+
+  buscarViviendas(event: Event) {
+    this.viviendas = this.viviendaService.buscarVivienda(this.query)
+  }
+  obetenerViviendas(){
+    return this.viviendas
+  }
+
   eliminarVivienda(id: number) {
     this.viviendaService.eliminarVivienda(id)
   }
   actualizarVivienda(id: number) {
     this.router.navigate(['manejar-vivienda', id]);
   }
-
 }
