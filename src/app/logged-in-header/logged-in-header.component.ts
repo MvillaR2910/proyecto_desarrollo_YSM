ee43cbf01d2b7402e3de829167bafa5cd534416e
sebastian <sebasruizr05@gmail.com>
2024-10-03 16:26:20 -0500

    Se agrega Bienvenida para el usuario

--- src/app/logged-in-header/logged-in-header.component.ts
+++ src/app/logged-in-header/logged-in-header.component.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
-import { Router, RouterModule } from '@angular/router'; // Importa RouterModule para standalone
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common'; // Importar CommonModule
+import { AuthService } from '../services/auth.service';  // Importa el servicio de autenticación
 
 @Component({
   selector: 'app-logged-in-header',
-  standalone: true,  // Marcar como standalone
-  imports: [RouterModule], // Asegúrate de importar RouterModule
+  standalone: true,
+  imports: [RouterModule, CommonModule], // Asegúrate de importar el CommonModule
   templateUrl: './logged-in-header.component.html',
   styleUrls: ['./logged-in-header.component.css']
 })
-export class LoggedInHeaderComponent {
-  constructor(private router: Router) {}
+export class LoggedInHeaderComponent implements OnInit {
+  userProfile: any;
+
+  constructor(private router: Router, private authService: AuthService) {}
+
+  ngOnInit() {
+    this.userProfile = this.authService.getUser();  // Recupera los datos del usuario
+  }
 
   logout() {
     localStorage.removeItem('userProfile');
@@ -17,7 +25,4 @@ export class LoggedInHeaderComponent {
     this.router.navigate(['/login']);
     window.location.reload(); 
   }
-  
 }
-
- 
