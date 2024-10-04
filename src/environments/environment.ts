e4693c94438fa83c1a929000c59d599510e4c08c
sebastian <sebasruizr05@gmail.com>
2024-10-03 21:59:31 -0500

    salvacion jesus

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    supabaseConfig:{
+        url: 'https://ocmefpjbkckbnvobewre.supabase.co',
+        apiKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9jbWVmcGpia2NrYm52b2Jld3JlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc5OTY0MDgsImV4cCI6MjA0MzU3MjQwOH0.2Qss_OlRmMhF9Jgo57HpcRmSA0cvWMGti4oqlJFTnwE'
+    }
+};
+
