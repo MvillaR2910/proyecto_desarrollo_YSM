731139913375e9f71b2d04bba83280ad81887076
yos <ysmaprogramming@gmail.com>
2024-11-09 18:32:37 -0500

    separación de front y back

--- front-viviendas/src/app/login/login.component.ts
+++ front-viviendas/src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+oimport { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
